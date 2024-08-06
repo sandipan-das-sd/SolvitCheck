286ade12a17f9c0b046ea1cbda5fdebadee5fb46
Sandipan Das <153122580+sandipan-das-sd@users.noreply.github.com>
2024-08-06 12:55:57 +0530

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -28,7 +28,7 @@ app.use(fileUpload({
 }));
 app.use(
     cors({
-        origin: ["http://solvit.live","https://solvit.live"],
+        origin: ["https://solvit.live"],
         methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
